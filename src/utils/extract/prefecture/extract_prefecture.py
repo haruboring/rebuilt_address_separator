3645858008a8fc4e4efd5aeead7e7f03be43f134
haruboting <raiu_hk@icloud.com>
2023-01-28 14:59:14 +0900

    add comment

--- src/utils/extract/prefecture/extract_prefecture.py
+++ src/utils/extract/prefecture/extract_prefecture.py
@@ -3,6 +3,7 @@ from data.prefecture_names import PREFECTURE_NAMES
 
 def extract_prefecture(address: str) -> tuple[str, str]:
 
+    # 愛媛県伊予市.... 愛媛県民ホールなどで不適切に分割されてしまうので、先頭からのre.matchで一致不一致を見分ける
     for prefecture in PREFECTURE_NAMES:
         if prefecture in address:
             return (prefecture, address.replace(prefecture, ""))
--- tests/utils/extract/prefecture/test_extract_prefecture.py
+++ tests/utils/extract/prefecture/test_extract_prefecture.py
@@ -4,7 +4,12 @@ from src.utils.extract.prefecture.extract_prefecture import extract_prefecture
 
 
 class TestFillSpace(TestCase):
-    def test_extract_prefecture(self) -> None:
+    def test_extract_prefecture1(self) -> None:
         prefecture, extra_address = extract_prefecture("東京都大田区")
         self.assertEqual(prefecture, "東京都")
         self.assertEqual(extra_address, "大田区")
+
+    def test_extract_prefecture2(self) -> None:
+        prefecture, extra_address = extract_prefecture("愛媛県愛媛東京都大田区")
+        self.assertEqual(prefecture, "愛媛県")
+        self.assertEqual(extra_address, "愛媛東京都大田区")
