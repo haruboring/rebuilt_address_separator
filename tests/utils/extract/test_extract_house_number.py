79a7c858cf93ef3bb8a025fb14a18fbf4aa2dd70
haruboting <raiu_hk@icloud.com>
2023-01-30 17:05:28 +0900

    add  pytest

--- tests/utils/extract/test_extract_house_number.py
+++ tests/utils/extract/test_extract_house_number.py
@@ -7,3 +7,7 @@ class TestExtractHouseNumber(TestCase):
     def test_extract_house_number1(self) -> None:
         house_number_and_rest_address = extract_house_number("1/tyoume二-3山田ハウス")
         self.assertEqual(house_number_and_rest_address, ("1-2-3", "山田ハウス"))
+
+    def test_extract_house_number2(self) -> None:
+        house_number_and_rest_address = extract_house_number("1-3-1/gou")
+        self.assertEqual(house_number_and_rest_address, ("1-3-1", ""))
