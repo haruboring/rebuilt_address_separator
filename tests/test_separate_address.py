f37bf1d08e478c168e9660e221745d40e4b6e708
haruboting <raiu_hk@icloud.com>
2023-01-30 16:27:40 +0900

    add pytest

--- tests/test_separate_address.py
+++ tests/test_separate_address.py
@@ -14,3 +14,33 @@ class TestSeparateAddress(TestCase):
         self.assertEqual(separated_address.house_number, "1-2-3")
         self.assertEqual(separated_address.building_name, "百川ビル一丁目")
         self.assertEqual(separated_address.room_number, "222号室")
+
+    def test_separate_address2(self) -> None:
+        separated_address: SeparatedAddress = separate_address("愛媛県松番市一番町3丁目3ー6", "")
+        self.assertEqual(separated_address.original_address, "愛媛県松番市一番町3丁目3ー6")
+        self.assertEqual(separated_address.prefecture, "愛媛県")
+        self.assertEqual(separated_address.city, "松番市")
+        self.assertEqual(separated_address.town, "一番町")
+        self.assertEqual(separated_address.house_number, "3-3-6")
+        self.assertEqual(separated_address.building_name, "")
+        self.assertEqual(separated_address.room_number, "")
+
+    def test_separate_address3(self) -> None:
+        separated_address: SeparatedAddress = separate_address("東京都港区南青山7-1-7. C-Cube南青山ビル 6F", "")
+        self.assertEqual(separated_address.original_address, "東京都港区南青山7-1-7. C-Cube南青山ビル 6F")
+        self.assertEqual(separated_address.prefecture, "東京都")
+        self.assertEqual(separated_address.city, "港区")
+        self.assertEqual(separated_address.town, "南青山")
+        self.assertEqual(separated_address.house_number, "7-1-7")
+        self.assertEqual(separated_address.building_name, ".C-Cube南青山ビル")
+        self.assertEqual(separated_address.room_number, "6F")
+
+    def test_separate_address4(self) -> None:
+        separated_address: SeparatedAddress = separate_address("東京都大田区西六郷4-30-7音金六郷マンション1-C号", "")
+        self.assertEqual(separated_address.original_address, "東京都大田区西六郷4-30-7音金六郷マンション1-C号")
+        self.assertEqual(separated_address.prefecture, "東京都")
+        self.assertEqual(separated_address.city, "大田区")
+        self.assertEqual(separated_address.town, "西六郷")
+        self.assertEqual(separated_address.house_number, "4-30-7")
+        self.assertEqual(separated_address.building_name, "音金六郷マンション")
+        self.assertEqual(separated_address.room_number, "1-C号")
