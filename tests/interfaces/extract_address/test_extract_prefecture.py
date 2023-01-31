95ae1e3bb735d0a4eac1a95193fe18f641fa7fb3
haruboting <raiu_hk@icloud.com>
2023-01-31 23:09:56 +0900

    add pytest

--- tests/interfaces/extract_address/test_extract_prefecture.py
+++ tests/interfaces/extract_address/test_extract_prefecture.py
@@ -11,3 +11,7 @@ class TestFillSpace(TestCase):
     def test_extract_prefecture2(self) -> None:
         prefecture_and_rest_address = ExtractAddress.extract_prefecture("愛媛県松山市〇〇町東京都民会館愛媛県支部")
         self.assertEqual(prefecture_and_rest_address, ("愛媛県", "松山市〇〇町東京都民会館愛媛県支部"))
+
+    def test_extract_prefecture3(self) -> None:
+        prefecture_and_rest_address = ExtractAddress.extract_prefecture("桃山町桃月2-221")
+        self.assertEqual(prefecture_and_rest_address, ("", "桃山町桃月2-221"))
--- tests/interfaces/extract_address/test_extract_room_number.py
+++ tests/interfaces/extract_address/test_extract_room_number.py
@@ -7,3 +7,11 @@ class TestExtractRoomNumber(TestCase):
     def test_extract_room_number1(self) -> None:
         room_number = ExtractAddress.extract_room_number("321/gou室")
         self.assertEqual(room_number, "321号室")
+
+    def test_extract_room_number2(self) -> None:
+        room_number = ExtractAddress.extract_room_number("321")
+        self.assertEqual(room_number, "321")
+
+    def test_extract_room_number3(self) -> None:
+        room_number = ExtractAddress.extract_room_number("")
+        self.assertEqual(room_number, "")
