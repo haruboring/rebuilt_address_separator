06a475f7259983db514118f316864c473fd909d8
haruboting <raiu_hk@icloud.com>
2023-01-31 16:48:23 +0900

    create class

--- src/interfaces/separated_address.py
+++ src/interfaces/separated_address.py
@@ -1,11 +1,5 @@
 from interfaces.utils.check_type import check_type
-from utils.extract.extract_building_name import extract_building_name
-from utils.extract.extract_city import extract_city
-from utils.extract.extract_house_number import extract_house_number
-from utils.extract.extract_prefecture import extract_prefecture
-from utils.extract.extract_room_number import extract_room_number
-from utils.extract.extract_town import extract_town
-
+from interfaces.extract_address import ExtractAddress
 
 class SeparatedAddress:
     def __init__(self, preprocessed_address: str, original_address: str):
@@ -13,22 +7,22 @@ class SeparatedAddress:
 
         self.original_address: str = original_address
 
-        prefecture, rest_address = extract_prefecture(rest_address)
+        prefecture, rest_address = ExtractAddress.extract_prefecture(rest_address)
         self.prefecture: str = check_type(prefecture, str)
 
-        city, rest_address = extract_city(rest_address)
+        city, rest_address = ExtractAddress.extract_city(rest_address)
         self.city: str = check_type(city, str)
 
-        town, rest_address = extract_town(rest_address)
+        town, rest_address = ExtractAddress.extract_town(rest_address)
         self.town: str = check_type(town, str)
 
-        house_number, rest_address = extract_house_number(rest_address)
+        house_number, rest_address = ExtractAddress.extract_house_number(rest_address)
         self.house_number: str = check_type(house_number, str)
 
-        building_name, rest_address = extract_building_name(rest_address)
+        building_name, rest_address = ExtractAddress.extract_building_name(rest_address)
         self.building_name: str = check_type(building_name, str)
 
-        room_number = extract_room_number(rest_address)
+        room_number = ExtractAddress.extract_room_number(rest_address)
         self.room_number: str = check_type(room_number, str)
 
     def __str__(self):
--- tests/utils/extract/test_extract_town.py
+++ tests/utils/extract/test_extract_town.py
@@ -1,13 +1,13 @@
 from unittest import TestCase
 
-from utils.extract.extract_town import extract_town
+from interfaces.extract_address import ExtractAddress
 
 
 class TestExtractTown(TestCase):
     def test_extract_town1(self) -> None:
-        town_and_rest_address = extract_town("北千束1-1-1〇〇ビルディング321")
+        town_and_rest_address = ExtractAddress.extract_town("北千束1-1-1〇〇ビルディング321")
         self.assertEqual(town_and_rest_address, ("北千束", "1-1-1〇〇ビルディング321"))
 
     def test_extract_town2(self) -> None:
-        town_and_rest_address = extract_town("一/ban町1")
+        town_and_rest_address = ExtractAddress.extract_town("一/ban町1")
         self.assertEqual(town_and_rest_address, ("一番町", "1"))
